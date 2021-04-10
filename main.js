b42cc13743b761f88f584877ca96e07010d6c0ed
SelesPro <registrospam@hotmail.com>
2021-04-10 22:51:43 +0200

    Inicial

---
+++ main.js
@@ -0,0 +1,16 @@
+// function comenzar(){
+//     zonadatos=document.getElementById("zonadatos");
+//     var archivos = document.getElementById("archivos");
+//     archivos.addEventListener("change", procesar, false);
+// }
+
+// function procesar(e){
+//     var archivos =e.target.files;
+//     var mi_archivo=archivos[0];
+//     var lector = new FileReader();
+//     lector.readAsText(mi_archivo);
+//     lector.addEventListener("load", mostrar_en_web, false);
+// }
+
+// window.addEventListener("load", comenzar, false);
+
